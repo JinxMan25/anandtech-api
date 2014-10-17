0dfa0a9f768374185447497a31ba53443710129c
Md Samiul Huq <samiulg3@gmail.com>
2014-10-17 14:54:46 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AnandtechApi::Application.config.secret_token = 'a63c3cce76bca7770ab4b4b09c6e1e5dd1c8083295692a1f9b8d70688bd8ecf7d5c00f626249c41503f9cb288418f8f3c6d3b8e4f9e759aed8b5d924441586ce'
