7a4b3119c0f2014750c50333c53437638f053740
Md Samiul Huq <samiulg3@gmail.com>
2014-10-18 10:21:35 -0400

    extract article title

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,5 +19,14 @@ class ArticlesController < ApplicationController
 
   def scrape_articles(doc)
     
+    @data = []
+
+    doc.css(".l_").each do |article_container|
+      article_title = article_container.css("h2").text
+    
+    temp_cell = { :title => article_title }
+    @data << temp_cell
+    end
+    @data
   end
 end
