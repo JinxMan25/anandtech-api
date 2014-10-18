19034efb6e47b2d3f6b3c6df6b2b834e59d76207
Md Samiul Huq <samiulg3@gmail.com>
2014-10-18 10:11:48 -0400

    fetch cache when retrieving articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,23 @@
 class ArticlesController < ApplicationController
+  require 'open-uri'
+  def articles
+
+  end
+
+
+  private
+
+  def get_articles
+    require 'open-uri'
+
+    doc = Nokogiri::HTML(open("http://anandtech.com"))
+
+    @articles = Rails.cache.fetch("anandtech/articles/v1") do
+      scrape_articles(doc)
+    end
+  end
+
+  def scrape_articles(doc)
+    
+  end
 end
